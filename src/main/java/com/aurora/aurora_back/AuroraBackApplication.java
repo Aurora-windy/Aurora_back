ae6ceb720f125f8ea5ece1529a16c5f23be32367
Aurora <1543277904@qq.com>
2026-05-27 20:36:09 +0800

    初始化

---
+++ src/main/java/com/aurora/aurora_back/AuroraBackApplication.java
@@ -0,0 +1,13 @@
+package com.aurora.aurora_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuroraBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aurora/aurora_back/AuroraBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurora.aurora_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
